94b90b64824b7036e700afb8367bb588c13b1e28
Ben Thornton <smenjas@users.noreply.github.com>
2020-01-14 12:54:15 -0800

    Add a logging example.

---
+++ logging-example.py
@@ -0,0 +1,34 @@
+import logging
+import math
+
+# Create and configure logger
+LOG_FORMAT = "%(Levelname)s %(asctime)s - %(message)s"
+logging.basicConfig(filename = example_log.txt,
+                    level = logging.DEBUG,
+                    format = LOG_FORMAT,
+                    filemode = 'a')
+logger = logging.getLogger()
+
+# Test messages
+logger.debug("debug")
+logger.info("info")
+logger.warning("warning")
+logger.error("error")
+logger.critical("critical")
+
+def quadratic_formula(a, b, c):
+    """Return the solutions to the equation ax^2 + bx + c = 0."""
+    logger.info("quadratic formula({0}, {1}, {2})").format(a, b, c)
+
+    # Compute the discriminant.
+    disc = b**2 - 4*a*c
+
+    # Compute the two roots
+    root1 = (-b + math.sqrt(disc)) / (2*a)
+    root2 = (-b - math.sqrt(disc)) / (2*a)
+
+    # Return the roots
+    return (root1, root2)
+
+roots = quadratic_formula(1, 0, -4)
+print(roots)
